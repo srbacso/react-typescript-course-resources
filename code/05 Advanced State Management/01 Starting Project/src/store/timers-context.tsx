59ba7bbdd6943af572ab0bcec0ee0c21bc1d7156
Stephen Bacso <stephen.bacso@gmail.com>
2025-02-11 15:09:42 -0500

    updated some projects

--- code/05 Advanced State Management/01 Starting Project/src/store/timers-context.tsx
+++ code/05 Advanced State Management/01 Starting Project/src/store/timers-context.tsx
@@ -1,4 +1,4 @@
-import {createContext} from 'react';
+import {type ReactNode, createContext, useContext, useReducer} from 'react';
 
 type Timer = {
   name: string;
@@ -11,6 +11,11 @@ type TimersState = {
 
 }
 
+const initialState: TimersState = {
+  isRunning: false,
+  timers: [],
+}
+
 type TimersContextValue = TimersState & {
   addTimer: (timer: Timer) => void;
   removeTimer: (timer: Timer) => void;
@@ -20,3 +25,39 @@ type TimersContextValue = TimersState & {
 
 const TimersContext = createContext<TimersContextValue | null>(null);
 
+export const useTimersContext = () => {
+  const timersCtx = useContext(TimersContext);
+  if (timersCtx === null) {
+    throw new Error('TimersContext is null');
+  }
+  return timersCtx;
+}
+
+type TimersContextProviderProps = {
+  children: ReactNode;
+}
+
+function TimerContextProvider({children}: TimersContextProviderProps) {
+  const [, ] = useReducer(, initialState);
+
+  const ctx: TimersContextValue = {
+    timers: [],
+    isRunning: false,
+    addTimer: () => {
+    },
+    removeTimer: () => {
+    },
+    startTimer: () => {
+    },
+    stopTimer: () => {
+    }
+  }
+
+  return (
+    <TimersContext.Provider value={ctx}>
+      {children}
+    </TimersContext.Provider>
+  )
+}
+
+export default TimerContextProvider;
